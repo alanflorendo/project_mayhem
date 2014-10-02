e51c2063771120dc6fbba27f17a55ae294e61da8
alanflorendo <alan.florendo@gmail.com>
2014-10-02 19:46:16 -0400

    add migration for hypo

--- mayhem/db/migrate/20141002234229_create_hypos.rb
+++ mayhem/db/migrate/20141002234229_create_hypos.rb
@@ -1,6 +1,16 @@
 class CreateHypos < ActiveRecord::Migration
   def change
     create_table :hypos do |t|
+    	t.string :law_name
+    	t.date :law_sponsor_date
+    	t.string :sponsor_name
+    	t.string :sponsor_city
+    	t.string :sponsor_state
+    	t.string :precip_amount
+    	t.boolean :is_tornado
+    	t.boolean :is_hurricane
+    	t.boolean :is_flood
+    	t.integer :vote_count
 
       t.timestamps
     end
