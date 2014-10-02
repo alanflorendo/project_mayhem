167e95ae30bd2c63d34fbae40c4c0564bbc742f2
alanflorendo <alan.florendo@gmail.com>
2014-10-02 19:48:10 -0400

    add migration for comment

--- mayhem/db/migrate/20141002234235_create_comments.rb
+++ mayhem/db/migrate/20141002234235_create_comments.rb
@@ -1,6 +1,11 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+    	t.string :text
+      t.integer :hypo_id
+      t.integer :user_id
+      t.integer :vote_count, default: 0
+
 
       t.timestamps
     end
