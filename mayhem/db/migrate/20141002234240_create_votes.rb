b691879d7ca319694fe4c53d9628650e2bbc5245
alanflorendo <alan.florendo@gmail.com>
2014-10-02 19:51:04 -0400

    add migration for vote

--- mayhem/db/migrate/20141002234240_create_votes.rb
+++ mayhem/db/migrate/20141002234240_create_votes.rb
@@ -1,6 +1,9 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
+      t.references :user
+      t.integer :votable_id, polymorphic: true
+      t.string :votable_type    	
 
       t.timestamps
     end
