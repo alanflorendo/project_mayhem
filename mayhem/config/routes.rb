5d7afac2c944fc7f3291e86d2ab20acb96d24ef8
alanflorendo <alan.florendo@gmail.com>
2014-10-03 18:15:33 -0400

    add users routes and login/logout/signup routes

--- mayhem/config/routes.rb
+++ mayhem/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'hypos#index'
   resources :hypos
+  resources :users, only: [:new, :create] #:destroy]
+
+  get 'login' => 'sessions#new', as: :login
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy', as: :logout
+  get 'signup' => 'users#new', as: :signup
+  post 'signup' => 'users#create'
 
 end
