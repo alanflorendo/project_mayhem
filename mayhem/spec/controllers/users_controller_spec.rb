4e5f1b7db04b00dbea9064fb145ee831927da411
alanflorendo <alan.florendo@gmail.com>
2014-10-03 19:04:11 -0400

    add user_controller_spec

--- mayhem/spec/controllers/users_controller_spec.rb
+++ mayhem/spec/controllers/users_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, :type => :controller do
+describe UsersController do 
+	let!(:user) { User.create!(email: "alann@gmail.com", password:"alann") }
 
+	describe "GET #new" do 
+		it "initializes a new user" do
+			get :new
+			expect(assigns(:user)).to be_a User
+		end
+	end
+
+	describe "POST #create" do 
+		context "when valid params are passed" do 
+			it "creates a new user" do 
+				post :create, user: FactoryGirl.attributes_for(:user)
+			end
+		end
+	end
 end
