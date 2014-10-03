ddff9bd933b1a0a9aaa7d18ead89b6cb9bf52bba
alanflorendo <alan.florendo@gmail.com>
2014-10-02 21:19:31 -0400

    add validations for user model (password, email, username)

--- mayhem/app/models/user.rb
+++ mayhem/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+	validates :email, presence: true
+  validates :username, presence: true
+  validates :password, presence: true
+  validates_uniqueness_of :username
+  validates_uniqueness_of :email
+  validates_confirmation_of :password, message: "Password doesn't match"
 
   has_many :hypos
   has_many :comments, through: :hypos
