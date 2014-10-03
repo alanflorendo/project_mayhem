d84989ddbba4e999d0b4b2e798c9af0ddf24116e
alanflorendo <alan.florendo@gmail.com>
2014-10-03 16:51:36 -0400

    add seeds file

--- mayhem/db/seeds.rb
+++ mayhem/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+5.times do
+  User.create(username: Faker::Internet.user_name, email: Faker::Internet.email, password: "password")
+end
+
+User.all.each do |user|
+  10.times do
+    user.hypos << Hypo.create(title: Faker::Lorem.sentence, sponsor_name: Faker::Name.name, sponsor_city: Faker::Address.city)
+  end
+end
