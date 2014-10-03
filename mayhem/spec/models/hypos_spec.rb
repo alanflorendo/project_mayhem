1e6db1f5cd6807f72ff51f9a2e73cc7306b7a5f3
alanflorendo <alan.florendo@gmail.com>
2014-10-03 16:10:56 -0400

    add validation for hypo model

--- mayhem/spec/models/hypos_spec.rb
+++ mayhem/spec/models/hypos_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Hypos, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Hypo do
+  it {should validate_presence_of(:title)}
+  # it {should validate_presence_of(:url)}
 end
