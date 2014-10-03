9f1d0cee460267043bfb84ef81701347b4839644
alanflorendo <alan.florendo@gmail.com>
2014-10-03 16:14:37 -0400

    validate email, username, password for user and add has_secure_password

--- mayhem/app/models/user.rb
+++ mayhem/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+	validates :email, presence: true
+  validates :username, presence: true
+  validates :password, presence: true
   validates_uniqueness_of :username
   validates_uniqueness_of :email
   validates_confirmation_of :password, message: "Password doesn't match"
@@ -7,5 +10,6 @@ class User < ActiveRecord::Base
   has_many :comments, through: :hypos
   has_many :votes
 
+  has_secure_password
 
 end
