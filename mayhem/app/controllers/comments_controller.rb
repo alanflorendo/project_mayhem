e0104d06dd05851d0eb53d6473d502d4f79bbac9
alanflorendo <alan.florendo@gmail.com>
2014-10-04 13:34:14 -0400

    add Comments#create controller method/route

--- mayhem/app/controllers/comments_controller.rb
+++ mayhem/app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+
+  def create
+    @hypo = Hypo.find(params[:hypo_id])
+    @comment = @hypo.comments.build(comment_params)
+    @comment.update(user_id: current_user.id)
+    20.times { puts "Comment Controller: CREATE"}
+    p params
+    if @comment.save
+      redirect_to @hypo
+    else
+      @comments = @hypo.comments
+      render :template => 'hypos/show'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text, :hypo_id, :user_id)
+  end
+
 end
