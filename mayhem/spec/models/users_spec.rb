d59e0a4de0920a0220da3b6c5609e259a8e04975
alanflorendo <alan.florendo@gmail.com>
2014-10-04 08:25:26 -0400

    add spec for user validations

--- mayhem/spec/models/users_spec.rb
+++ mayhem/spec/models/users_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 describe User do
   it {should have_many(:photos)}
   it {should validate_presence_of(:email)}
+  it {should validate_presence_of(:username)}
   it {should validate_presence_of(:password)}
+  it {should validate_uniqueness_of(:email)}
+  it {should validate_uniqueness_of(:username)}
 
   let!(:user) { User.create(email:"alan@gmail.com", username: "alan", password:"alan") }
 	context "email" do 
