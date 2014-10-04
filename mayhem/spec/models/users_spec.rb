94db0f24f3402bb56b40886796fc76ff0420054c
alanflorendo <alan.florendo@gmail.com>
2014-10-04 08:28:12 -0400

    add validation for has_secure_password

--- mayhem/spec/models/users_spec.rb
+++ mayhem/spec/models/users_spec.rb
@@ -7,6 +7,7 @@ describe User do
   it {should validate_presence_of(:password)}
   it {should validate_uniqueness_of(:email)}
   it {should validate_uniqueness_of(:username)}
+  it {should have_secure_password }
 
   let!(:user) { User.create(email:"alan@gmail.com", username: "alan", password:"alan") }
 	context "email" do 
