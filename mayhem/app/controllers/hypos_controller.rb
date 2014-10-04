76aece9346b55a96bc4b87142374119380f22700
alanflorendo <alan.florendo@gmail.com>
2014-10-04 10:06:42 -0400

    add show view for hypos (and also add routes for hypoController)

--- mayhem/app/controllers/hypos_controller.rb
+++ mayhem/app/controllers/hypos_controller.rb
@@ -4,4 +4,23 @@ class HyposController < ApplicationController
 		@hypos = Hypo.all
 	end
 
+	def new
+		@hypo = Hypo.new
+	end
+
+	def show
+		@hypo = Hypo.find(params[:id])
+	end
+
+	def create
+		@hypo = Hypo.new(hypo_params)
+	end
+
+	private
+
+	def hypo_params
+		params.require(:title, :law_name, :law_sponsor_date, :sponsor_name, :sponsor_city, :sponsor_state, :precip_amount, :is_tornado, :is_hurricane, :is_flood, :vote_count
+, :user_id).permit()
+	end
+
 end
