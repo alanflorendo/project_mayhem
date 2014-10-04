5f74ca71867f524a999935f1d942240c53eda987
alanflorendo <alan.florendo@gmail.com>
2014-10-04 12:17:51 -0400

    implement initial version of Vote#create controller method

--- mayhem/app/controllers/votes_controller.rb
+++ mayhem/app/controllers/votes_controller.rb
@@ -1,2 +1,14 @@
 class VotesController < ApplicationController
+
+  def create
+    @hypos = Hypo.all
+    hypo = Hypo.find(params[:hypo_id])
+    vote = hypo.votes.new(user_id: session[:user_id], votable_id: hypo.id, votable_type: "Hypo", value: 1)
+    if vote.save
+      render 'hypos/index' and return
+    else
+      render 'hypos/index' and return
+    end
+  end
+
 end
