749c666eed6c949d6c0c654f0bf02fca0318d05f
alanflorendo <alan.florendo@gmail.com>
2014-10-04 08:20:36 -0400

    add spec for user.email

--- mayhem/spec/models/users_spec.rb
+++ mayhem/spec/models/users_spec.rb
@@ -4,4 +4,12 @@ describe User do
   it {should have_many(:photos)}
   it {should validate_presence_of(:email)}
   it {should validate_presence_of(:password)}
+
+  let!(:user) { User.create(email:"alan@gmail.com", username: "alan", password:"alan") }
+	context "email" do 
+		it "should return valid email" do  
+			expect(user.email).to eq("alan@gmail.com")
+		end
+	end
+
 end
